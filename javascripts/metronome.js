10a54ac0edc75ac23f28249a7c8dc50ea0a20f27
Christian Wörner <christianworner@gmail.com>
2013-01-23 22:35:08 +0200

    adds initial visualization

--- javascripts/metronome.js
+++ javascripts/metronome.js
@@ -1,7 +1,11 @@
+rhythm = {weightNames:{1:"heavy", 2:"light", 3:"fill"}}
 function playMetro(){
     window.setTimeout(function(){
         if (!state.pauseD) {
             playBeat(state.meter[state.position]);
+            drawRhythm();
+            $('.meterItem').removeClass('highlight')
+            $('#meterItem_' + state.position).addClass('highlight')
             state.position = (state.position + 1) % state.meter.length
             playMetro();
         }
--- javascripts/rhythmViewPort.js
+++ javascripts/rhythmViewPort.js
@@ -1,7 +1,34 @@
-fullHeight = document.height - 100;
-heightMult = document.height / 100;
-slotWidth = document.width * 0.8 / (document.max_queue_length * 1.1);
+viewPort = {
+    fullHeight: document.height - 250,
+    heightMult: document.height / 180,
+    slotWidth: document.width * 0.05
+}
 
 function drawRhythm(){
+    if (!state.displayRhythM){
+        $('#rhythmViewPort').hide();
+        return false;
+    }
+    $.each(state.meter, function(idx, weight){
+        var eles = getOrCreate(idx, weight)
+        eles.text(idx).css({"left": idx * viewPort.slotWidth,
+                                 "top": viewPort.fullHeight - (weight * 20 * viewPort.heightMult),
+                                 "width": viewPort.slotWidth,
+                                });
+})
 
+}
+
+function getOrCreate(idx, weight){
+    var id =  "meterItem_" + idx;
+    var eles = $('#' + id)
+        if (eles.length == 0){
+            var ele = $('#rhythmViewPort').append("<span id=" + id + "></span>");
+            var eles = $('#' + id);
+            eles.addClass(rhythm.weightNames[weight])
+            //eles.css({"background-Color": document.colors[weight], "position":"absolute"})
+            eles.css({"position":"absolute"})
+        }
+    eles.addClass("meterItem");
+    return eles
 }
