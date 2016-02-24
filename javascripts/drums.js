be1413c2a199b52f8b2be5a97a9d857253144986
Christian Wörner <christianworner@gmail.com>
2016-02-24 01:10:54 +0100

    JS: readability

--- javascripts/drums.js
+++ javascripts/drums.js
@@ -8,15 +8,15 @@ aGraph.DRUM_BUFFER_NAMES = [
 aGraph.setUpDrumGraph = function(){
     aGraph.gainNodeDrums = context.createGain();
     aGraph.gainNodeDrums.gain.value = 2;
-    aGraph.gainNodeDrums.connect(aGraph.gainNodeAll)
-    for (bufferNameIdx in aGraph.DRUM_BUFFER_NAMES){
-        bufferName = aGraph.DRUM_BUFFER_NAMES[bufferNameIdx]
+    aGraph.gainNodeDrums.connect(aGraph.gainNodeAll);
+    for (bufferNameIdx in aGraph.DRUM_BUFFER_NAMES) {
+        bufferName = aGraph.DRUM_BUFFER_NAMES[bufferNameIdx];
         var panner = context.createPanner(),
             gain = context.createGain();
         panner.setPosition(0, 0, 0);
         aGraph[bufferName + "Panner"] = panner;
         aGraph[bufferName + "GainNode"] = gain;
-        gain.connect(panner)
+        gain.connect(panner);
         panner.connect(aGraph.gainNodeDrums);
     }
     aGraph["kickBufferGainNode"].gain.value = 0.8;
--- javascripts/rhythmViewPort.js
+++ javascripts/rhythmViewPort.js
@@ -4,31 +4,35 @@ $(document).ready(function(){
     viewPort.fullWidth= Number($('#rhythmViewPort').css('width').match(/[0-9]+/));
     viewPort.heightMult=  viewPort.fullHeight / 180;
 
-    viewPort.circle = {x_offset:  0.45 * viewPort.fullWidth,
-                       x_stretch: 0.17 *  viewPort.fullWidth,
-                       y_offset:  -0.2 * viewPort.fullHeight,
-                       y_stretch: 1.3 * viewPort.fullHeight,
-                       radius:{'heavy': 1.8, 'light':1.6, 'fill':1.2}}
+    viewPort.circle = {
+        x_offset:  0.45 * viewPort.fullWidth,
+        x_stretch: 0.17 *  viewPort.fullWidth,
+        y_offset:  -0.2 * viewPort.fullHeight,
+        y_stretch: 1.3 * viewPort.fullHeight,
+        radius: {
+            'heavy': 1.8, 'light':1.6, 'fill':1.2
+        }
+    }
 
-    viewPort.drawRhythm = function(){
+    viewPort.drawRhythm = function () {
         viewPort.slotWidth= viewPort.fullWidth / rhythm.meter.length;
-        viewPort.linear = {fullSlotWidth: viewPort.slotWidth}
-        if (!state.displayRhythM){
+        viewPort.linear = {fullSlotWidth: viewPort.slotWidth};
+        if (!state.displayRhythM) {
             $('#rhythmViewPort').hide();
             return false;
         }
         $('.meterItem').hide();
-        $.each(rhythm.meter, function(idx, weight){
+        $.each(rhythm.meter, function(idx, weight) {
             var eles = viewPort.getOrCreate(idx, weight)
-            $.each(rhythm.weightNames, function(iidx, weight){eles.removeClass(weight)});
-            eles.addClass(rhythm.weightNames[weight])
+            $.each(rhythm.weightNames, function(iidx, weight) {eles.removeClass(weight)});
+            eles.addClass(rhythm.weightNames[weight]);
             eles.show();
             if (viewPort.proportional){
                 var radius = 0.55 * viewPort.slotWidth * viewPort.circle.radius[rhythm.weightNames[weight]];
             } else {
-                var radius = 0.55 * viewPort.slotWidth * 1.75
+                var radius = 0.55 * viewPort.slotWidth * 1.75;
             }
-            if (viewPort.linear_draw){
+            if (viewPort.linear_draw) {
                 eles.text(idx).css({"left": idx * viewPort.linear.fullSlotWidth,
                                     "top": viewPort.fullHeight - (rhythm.weightToPosition[weight] * 60 * viewPort.heightMult),
                                     "width": radius,
@@ -49,30 +53,29 @@ $(document).ready(function(){
                                     "text-align":"center"
                                   });
             }
-    })
-
+        })
     }
 
-    viewPort.getOrCreate = function(idx, weight){
-        var id =  "meterItem_" + idx;
-        var eles = $('#' + id)
+    viewPort.getOrCreate = function (idx, weight) {
+        var id = "meterItem_" + idx;
+        var eles = $('#' + id);
             if (eles.length == 0){
                 var ele = $('#rhythmViewPort').append("<span id=" + id + "></span>");
                 var eles = $('#' + id);
-                eles.css({"position":"absolute"})
+                eles.css({"position":"absolute"});
             }
         eles.addClass("meterItem");
-        return eles
+        return eles;
     }
 
-    viewPort.drawFullscreenAtPos = function(pos){
+    viewPort.drawFullscreenAtPos = function (pos) {
         viewPort.resetFullscreen();
         var body = $('body');
         var klass = rhythm.weightNames[rhythm.meter[pos]];
         body.addClass(klass);
     }
 
-    viewPort.resetFullscreen = function(){
+    viewPort.resetFullscreen = function () {
         var body = $('body');
         $.each(rhythm.weightNames, function(idx, weight){body.removeClass(weight)});
     }
