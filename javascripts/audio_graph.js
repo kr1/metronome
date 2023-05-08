40049a38f95d4341daef9b8709adae399167f819
Christian Wörner <christianworner@gmail.com>
2023-05-08 13:02:38 +0200

    audio graph: add new nodes

--- javascripts/audio_graph.js
+++ javascripts/audio_graph.js
@@ -11,6 +11,28 @@ aGraph.setUpAudioGraph = function() {
     aGraph.gainNodeAll = context.createGain();
     aGraph.gainNodeAll.gain.value = 0.2;
     aGraph.setUpDrumGraph();
+
+    aGraph.gainNodeDrone = context.createGain();
+    aGraph.oscillator.connect(aGraph.osc_biquadFilter1);
+    aGraph.oscillator.connect(aGraph.osc_biquadFilter2);
+    aGraph.oscillator.connect(aGraph.osc_biquadFilter2);
+
+    aGraph.osc_biquadFilter1.connect(aGraph.gainNodeDrone);
+    aGraph.osc_biquadFilter2.connect(aGraph.gainNodeDrone);
+    aGraph.osc_biquadFilter3.connect(aGraph.gainNodeDrone);
+
+    // TODO: implement LFO based filter frequency
+    //aGraph.lfo_filter1.connect(aGraph.lfo_filter1_gain);
+    //aGraph.lfo_filter1_gain.connect(aGraph.osc_biquadFilter1.frequency);
+
+    //aGraph.lfo_filter2.connect(aGraph.lfo_filter2_gain);
+    //aGraph.lfo_filter2_gain.connect(aGraph.osc_biquadFilter2.frequency);
+
+    //aGraph.lfo_filter3.connect(aGraph.lfo_filter3_gain);
+    //aGraph.lfo_filter3_gain.connect(aGraph.osc_biquadFilter3.frequency);
+
+    aGraph.gainNodeDrone.gain.value = aGraph.gainNodeDrone_initial || 0.5;
+    aGraph.gainNodeDrone.connect(aGraph.gainNodeAll);
     // connect global gain to destination
     aGraph.gainNodeAll.connect(context.destination);
 }
