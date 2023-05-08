2bb915c5889d71479ebf378e1fa1c9ecd5cf3878
Christian Wörner <christianworner@gmail.com>
2023-05-08 12:09:46 +0200

    audio: add oscillator and filters for drone

--- javascripts/audio.js
+++ javascripts/audio.js
@@ -33,6 +33,52 @@ function createAudioContext () {
     } catch(ReferenceError) {
         alert("metronome works only on browsers that implement the Web Audio API: e.g.: Firefox & Chrome")
     }
+    aGraph.oscillator = context.createOscillator();
+
+    aGraph.oscillator.type = "square";
+    aGraph.oscillator.frequency.setValueAtTime(440, context.currentTime); // value in hertz
+
+    aGraph.osc_biquadFilter1 = context.createBiquadFilter();
+    aGraph.osc_biquadFilter1.type = 'bandpass';
+    aGraph.osc_biquadFilter1.Q.value = 13;
+    aGraph.osc_biquadFilter1.gain.value = 4;
+    aGraph.osc_biquadFilter1.frequency.value = 401;
+    aGraph.osc_biquadFilter2 = context.createBiquadFilter();
+    aGraph.osc_biquadFilter2.type = 'bandpass';
+    aGraph.osc_biquadFilter2.Q.value = 14;
+    aGraph.osc_biquadFilter2.gain.value = 4;
+    aGraph.osc_biquadFilter2.frequency.value = 925;
+    aGraph.osc_biquadFilter3 = context.createBiquadFilter();
+    aGraph.osc_biquadFilter3.type = 'bandpass';
+    aGraph.osc_biquadFilter3.Q.value = 15;
+    aGraph.osc_biquadFilter3.gain.value = 40;
+    aGraph.osc_biquadFilter3.frequency.value = 1440;
+
+    // the original intention of using an LFO to control the filter frequencies
+    // has not been viable so far
+    //aGraph.lfo_filter1 = context.createOscillator();
+    //aGraph.lfo_filter1.type = 'triangle';
+    //aGraph.lfo_filter1.frequency.setValueAtTime(0.04, context.currentTime);
+    //aGraph.lfo_filter1_gain = context.createGain();
+    //aGraph.lfo_filter1_gain.value = 12400;
+
+    //aGraph.lfo_filter2 = context.createOscillator();
+    //aGraph.lfo_filter2.type = 'triangle';
+    //aGraph.lfo_filter2.frequency.setValueAtTime(0.31, context.currentTime);
+    //aGraph.lfo_filter2_gain = context.createGain();
+    //aGraph.lfo_filter2_gain.value = 4400;
+
+    //aGraph.lfo_filter3 = context.createOscillator();
+    //aGraph.lfo_filter3.type = 'triangle';
+    //aGraph.lfo_filter3.frequency.setValueAtTime(0.17, context.currentTime);
+    //aGraph.lfo_filter3_gain = context.createGain();
+    //aGraph.lfo_filter3_gain.value = 10800;
+
+    aGraph.filter_nodes = [
+        aGraph.osc_biquadFilter1,
+        aGraph.osc_biquadFilter2,
+        aGraph.osc_biquadFilter3,
+    ]
 
     aGraph.setUpAudioGraph(context);
 }
